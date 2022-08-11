a10a9413d60fb783c4f7032515fbc6d52d465397
Damiro Souza <damirodev@gmail.com>
2022-08-11 14:03:16 -0300

    Primeiro commit

---
+++ init.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+						   
+	$("#countdown").countdown({
+				date: "20 november 2022 12:00:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+});
